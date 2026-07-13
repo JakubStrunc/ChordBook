cfa4c06d317ca2d994d16916f04cc13601633a1f
jakubstrunc <kubaelis@gmail.com>
2026-07-13 19:50:38 +0200

    songs get endpoint

---
+++ ChordBook.Api/Endpoints/SongEndpoints.cs
@@ -0,0 +1,24 @@
+﻿using ChordBook.Services;
+
+namespace ChordBook.Endpoints;
+
+public static class SongEndpoints
+{
+    public static IEndpointRouteBuilder MapSongEndpoints(
+        this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapGet("/api/songs", async (
+                SongService songService,
+                CancellationToken cancellationToken) =>
+            {
+
+                var songs = await songService.GetSongsAsync(cancellationToken);
+
+                return Results.Ok(songs);
+
+            })
+            .RequireAuthorization();
+        
+        return endpoints;
+    }
+}
